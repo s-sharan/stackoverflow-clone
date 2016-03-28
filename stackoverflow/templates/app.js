aafc9166b3d4a84636c9d5f6c272b32f7056e563
Rakesh Yarlagadda <ry2294@columbia.edu>
2016-03-28 03:56:20 +0000

    Added ui-router

---
+++ stackoverflow/templates/app.js
@@ -0,0 +1,61 @@
+var app = angular.module('stackoverflowApp', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/login');
+  
+  $stateProvider.state('login', {
+    templateUrl: 'login.html',
+    url: '/login',
+    controller: 'loginController'
+  })
+  .state('search', {
+      templateUrl: 'search.html',
+      url: '/search',
+      controller: 'searchController'
+  })
+  .state('question', {
+    templateUrl: "question.html",
+    url: '/search',
+    controller: "questionController"
+  });
+});
+
+app.controller('navigationController', function($scope) {
+    console.log("Testing Navigation");
+});
+
+app.controller('loginController', function($scope, $state) {
+    $scope.authenticate = function() {
+        /* 
+            OAuth Stuff
+        */
+        
+        $state.go('search');
+    };
+});
+
+app.controller('questionController', function($scope, $rootScope, $state) {
+    console.log('Testing Question');
+    $scope.question = $rootScope.selectedQuestion;
+    $scope.back = function() {$state.go('search')};
+});
+
+app.controller('searchController', function($scope, $rootScope, $state) {
+    console.log('Testing Search');
+    $scope.questions = [{title: 'Is angular better than react ?', tags: ['C++', 'Java']}, 
+        {title: 'Is java better than C++ ?', tags: ['C++', 'Java']}, 
+        {title: 'Is javascript a better language ?', tags: ['C++', 'Java']}, 
+        {title: 'Is angular better than react ?', tags: ['C++', 'Java']},
+        {title: 'Is javascript a better language ?', tags: ['C++', 'Java']}, 
+        {title: 'Is angular better than react ?', tags: ['C++', 'Java']},
+        {title: 'Is javascript a better language ?', tags: ['C++', 'Java']}, 
+        {title: 'Is angular better than react ?', tags: ['C++', 'Java']},
+        {title: 'Is javascript a better language ?', tags: ['C++', 'Java']}, 
+        {title: 'Is angular better than react ?', tags: ['C++', 'Java']}];
+    console.log($scope.questions);
+    
+    $scope.viewQuestion = function(index) {
+        $rootScope.selectedQuestion = $scope.questions[index];
+        $state.go('question');
+    };
+});
