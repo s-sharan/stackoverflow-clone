4240bb0e2bd54119a1b93b3c9f3d2afaf1e5b6f0
Rakesh Yarlagadda <ry2294@columbia.edu>
2016-03-29 10:58:23 +0000

    Adding FB Login

--- stackoverflow/static/js/app.js
+++ stackoverflow/static/js/app.js
@@ -1,81 +1,22 @@
-  // This is called with the results from from FB.getLoginStatus().
-  function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      testAPI();
-    } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
-    } else {
-      // The person is not logged into Facebook, so we're not sure if
-      // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
-    }
-  }
 
-  // This function is called when someone finishes with the Login
-  // Button.  See the onlogin handler attached to it in the sample
-  // code below.
-  function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
+
+var app = angular.module('stackoverflowApp', ['ui.router', 'ngFacebook']);
+
+app.config(['$facebookProvider', function($facebookProvider) {
+    $facebookProvider.setAppId('532179143609395').setPermissions(['email','user_friends']);
+  }])
+  .run(['$rootScope', '$window', function($rootScope, $window) {
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    $rootScope.$on('fb.load', function() {
+      $window.dispatchEvent(new Event('fb.load'));
     });
-  }
-
-  window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '532179143609395',
-    cookie     : true,  // enable cookies to allow the server to access 
-                        // the session
-    xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.5' // use graph api version 2.5
-  });
-
-  // Now that we've initialized the JavaScript SDK, we call 
-  // FB.getLoginStatus().  This function gets the state of the
-  // person visiting this page and can return one of three states to
-  // the callback you provide.  They can be:
-  //
-  // 1. Logged into your app ('connected')
-  // 2. Logged into Facebook, but not your app ('not_authorized')
-  // 3. Not logged into Facebook and can't tell if they are logged into
-  //    your app or not.
-  //
-  // These three cases are handled in the callback function.
-
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-
-  };
-
-  // Load the SDK asynchronously
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-
-function testAPI() {
-console.log('Welcome!  Fetching your information.... ');
-FB.api('/me', function(response) {
-  console.log('Successful login for: ' + response.name);
-  document.getElementById('status').innerHTML =
-    'Thanks for logging in, ' + response.name + ' id = ' + response.id + '!';
-});
-}
-
-var app = angular.module('stackoverflowApp', ['ui.router']);
+  }]);
 
 app.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/login');
@@ -107,7 +48,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
   });
 });
 
-app.controller('navigationController', function($scope, $state, $rootScope) {
+app.controller('navigationController', function($scope, $state, $rootScope, $facebook) {
     console.log("Testing Navigation");
     
     $scope.logout = function() {$state.go('login');};
@@ -115,13 +56,25 @@ app.controller('navigationController', function($scope, $state, $rootScope) {
     $rootScope.user = {userid: "5969614"};
 });
 
-app.controller('loginController', function($scope, $state) {
-    $scope.authenticate = function() {
-        /* 
-            OAuth Stuff
-        */
-        
-        $state.go('search');
+app.controller('loginController', function($scope, $state, $facebook, $rootScope) {
+    $scope.$on('fb.auth.authResponseChange', function() {
+      $scope.status = $facebook.isConnected();
+      if($scope.status) {
+        $facebook.api('/me').then(function(user) {
+          $rootScope.user = user;
+          $scope.user = user;
+          console.log(user);
+          $state.go('search');
+        });
+      }
+    });
+
+    $scope.loginToggle = function() {
+      if($scope.status) {
+        $facebook.logout();
+      } else {
+        $facebook.login();
+      }
     };
 });
 
